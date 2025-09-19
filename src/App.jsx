6a6254b5a8c27456b7c61d72d817d384777eed12
Muhammad Shakir <127669286+MdShakir110@users.noreply.github.com>
2025-09-19 22:26:33 +0500

    job portal setup created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-white">
+        React + Vite + Tailwind v4 ✅
+      </h1>
+    </div>
+  )
+}
+
+export default App
