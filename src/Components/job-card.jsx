2c3b6dea42d46cf61f2248a1e8a10f8dbbc26d8f
Muhammad Shakir <127669286+MdShakir110@users.noreply.github.com>
2025-09-20 06:01:56 +0500

    job card added bg color with group

--- src/Components/job-card.jsx
+++ src/Components/job-card.jsx
@@ -1,60 +1,56 @@
-import {
-  FiBriefcase,
-  FiMapPin,
-  FiClock,
-  FiUsers,
-  FiBookmark,
-} from "react-icons/fi";
+import { FiMapPin, FiClock, FiUsers, FiBookmark } from "react-icons/fi";
 
 const JobCard = ({ job }) => {
   return (
-    <div className="bg-white  rounded-lg p-4 shadow-sm  hover:shadow-md transition-shadow">
+    <div className="bg-white rounded-lg p-4 shadow-sm group hover:bg-blue-600 transition-colors">
       {job.promoted && (
         <div className="flex items-center gap-1 mb-3">
-          <span className="text-xs bg-accent text-accent-foreground font-bold px-2 py-1 rounded">
+          <span className="text-xs bg-accent text-accent-foreground font-bold px-2 py-1 rounded group-hover:bg-blue-500 group-hover:text-white">
             Promoted
           </span>
         </div>
       )}
 
       <div className="flex items-center gap-3 mb-3">
-        <div className="w-10 h-10 bg-accent rounded-lg flex items-center justify-center overflow-hidden">
+        <div className="w-10 h-10 bg-gray-50 rounded-full flex items-center justify-center overflow-hidden">
           <img
-            src={job.companyImage} // company ki image ka URL
+            src={job.companyImage}
             alt={job.company}
             className="w-full h-full object-cover"
           />
         </div>
         <div>
-          <h3 className="font-normal text-card-foreground text-md">
+          <h3 className="font-normal text-card-foreground text-md group-hover:text-white">
             {job.title}
           </h3>
-          <p className="text-xs text-muted-foreground">{job.company}</p>
+          <p className="text-xs text-muted-foreground group-hover:text-white">
+            {job.company}
+          </p>
         </div>
       </div>
 
       <div className="space-y-2 mb-4">
-        <div className="flex items-center gap-1 text-xs text-muted-foreground">
-          <FiMapPin className="w-3 h-3 text-gray-400" />
-          <span className="text-gray-400">{job.location}</span>
+        <div className="flex items-center gap-1 text-xs text-muted-foreground group-hover:text-white">
+          <FiMapPin className="w-3 h-3 text-gray-400 group-hover:text-white" />
+          <span>{job.location}</span>
         </div>
-        <div className="flex items-center gap-2 ">
-          <div className="flex items-center gap-1 text-xs text-muted-foreground">
-            <FiClock className="w-3 h-3 text-gray-400" />
-            <span className="text-gray-400">{job.timePosted}</span>
+        <div className="flex items-center gap-2">
+          <div className="flex items-center gap-1 text-xs text-muted-foreground group-hover:text-white">
+            <FiClock className="w-3 h-3 text-gray-400 group-hover:text-white" />
+            <span>{job.timePosted}</span>
           </div>
-          <div className="flex items-center gap-1 text-xs text-muted-foreground">
-            <FiUsers className="w-3 h-3 text-gray-400" />
-            <span className="text-blue-600">{job.applicants}</span>
+          <div className="flex items-center gap-1 text-xs text-muted-foreground group-hover:text-white">
+            <FiUsers className="w-3 h-3 text-blue-600 group-hover:text-white" />
+            <span>{job.applicants}</span>
           </div>
         </div>
       </div>
-      <div className="flex  justify-between items-center gap-4">
-        <button className="py-2 cursor-pointer  bg-blue-600 text-white rounded-lg text-sm font-medium hover:bg-blue-800 transition-colors flex items-center justify-between px-8">
+
+      <div className="flex justify-between items-center gap-4">
+        <button className="py-2 cursor-pointer bg-blue-600 text-white rounded-lg text-sm font-medium group-hover:bg-white group-hover:text-blue-600 transition-colors flex items-center justify-between px-8">
           Apply Now
         </button>
-
-        <FiBookmark className="w-6 h-6 text-gray-300 hover:text-gray-500" />
+        <FiBookmark className="w-6 h-6 text-gray-300 group-hover:text-white" />
       </div>
     </div>
   );
